183643c1a3abe5c31d7fd50a1d4369b6f9cc51d7
sparrowsl <benthorpe.dev@gmail.com>
2023-07-03 23:19:30 +0000

    refator: update navigation component

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,3 +1,9 @@
+<script>
+	import { page } from "$app/stores";
+
+	console.log($page);
+</script>
+
 <div class="bg-gray-500 text-white font-roboto">
 	<nav class="container flex justify-between items-center min-h-10dvh">
 		<!-- Logo -->
@@ -6,8 +12,11 @@
 		<ul class="flex gap-5 items-center text-sm">
 			<li><a href="/login">Login</a></li>
 			<li><a href="/register">Register</a></li>
-			<!-- <li><a href="/">Home</a></li> -->
-			<!-- <li><a href="/">Home</a></li> -->
+			{#if $page?.locals}
+				<li><a href="/new">New Post</a></li>
+				<li><a href="/account">Account</a></li>
+				<li><a href="/logout">Logout</a></li>
+			{/if}
 		</ul>
 	</nav>
 </div>
