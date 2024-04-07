de5db858332d037c69629c7d6d739805c0093ac2
sparrowsl <benthorpe.dev@gmail.com>
2024-04-07 01:49:29 +0000

    update docker-compose - change container names

--- client/src/lib/pocketbase.js
+++ client/src/lib/pocketbase.js
@@ -1,6 +1,7 @@
+import { PUBLIC_API_URL } from "$env/static/public";
 import PocketBase from "pocketbase"
 
-const pb = new PocketBase('http://localhost:8090');
+const pb = new PocketBase(PUBLIC_API_URL);
 pb.autoCancellation(false)
 
 export default pb
